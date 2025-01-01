11a8db424c4c3b9ceb23e5a6f7281bce4efd20eb
LitApple <biweitong999@163.com>
2025-01-01 14:46:56 +0800

    更新了翻牌配对游戏的功能和样式

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const images = [
-    'images/img1.jpg', 'images/img1.jpg', 'images/img2.jpg', 'images/img2.jpg',
-    'images/img3.jpg', 'images/img3.jpg', 'images/img4.jpg', 'images/img4.jpg',
-    'images/img5.jpg', 'images/img5.jpg', 'images/img6.jpg', 'images/img6.jpg',
-    'images/img7.jpg', 'images/img7.jpg', 'images/img8.jpg', 'images/img8.jpg'
+    'images/1.png', 'images/1.png', 'images/2.png', 'images/2.png',
+    'images/3.png', 'images/3.png', 'images/4.png', 'images/4.png',
+    'images/5.png', 'images/5.png', 'images/6.png', 'images/6.png',
+    'images/7.png', 'images/7.png', 'images/8.png', 'images/8.png'
 ];
 
 let shuffledImages = [];
